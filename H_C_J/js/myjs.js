a2fe8d09098521cdc84c0da2c8555d9adfe101fb
icecream4k <969666859@qq.com>
2020-06-17 13:11:46 +0800

    6,17

--- H_C_J/js/myjs.js
+++ H_C_J/js/myjs.js
@@ -1,22 +1,22 @@
-function saoma_01(){
-	let c = document.getElementById('denglu_div_01')
-	c.innerHTML = `
-	<iframe src="./denglu_01.html" scrolling="no" height="350px"
+function saoma_01() {
+  let c = document.getElementById("denglu_div_01");
+  c.innerHTML = `
+	<iframe src="../html/denglu_01.html" scrolling="no" height="350px"
 	 width="355px"
 	 frameborder="0"
 	 margin-left:-5px
 	 align='center'></iframe>
-	`
+	`;
 }
-function saoma_02(){
-	let c = document.getElementById('denglu_div_01')
-	c.innerHTML = `
-	<iframe src="./denglu_02.html" scrolling="no" height="350px"
+function saoma_02() {
+  let c = document.getElementById("denglu_div_01");
+  c.innerHTML = `
+	<iframe src="../html/denglu_02.html" scrolling="no" height="350px"
 	 width="355px"
 	 frameborder="0"
 	 margin-left:-5px
 	 align='center'></iframe>
-	`
+	`;
 }
 //     let num = 1;
 //     let content = document.getElementById('content');
