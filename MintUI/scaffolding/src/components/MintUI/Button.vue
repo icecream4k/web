3e4b8dfc1460d3144c9ac394328ba9aae82aca8c
icecream4k <969666859@qq.com>
2020-06-18 01:20:08 +0800

    6,18

--- MintUI/scaffolding/src/components/MintUI/Button.vue
+++ MintUI/scaffolding/src/components/MintUI/Button.vue
@@ -1,7 +1,33 @@
+
 <template>
-    <div>
-        <p><mt-button type='default' size='small'>默认</mt-button></p>
-        <p><mt-button type='primary' size='normal' icon='more' disabled>主要[被disabled禁用]</mt-button></p>
-        <router-link to='/'><p><mt-button type='danger' size='large' icon='back'>危险</mt-button></p></router-link>
-    </div>
+  <div>
+    <p>
+      <mt-button type="default" size="small">默认</mt-button>
+    </p>
+    <p>
+      <mt-button type="primary" size="normal" icon="more" disabled>主要[被disabled禁用]</mt-button>
+    </p>
+    <router-link to="/">
+      <p>
+        <mt-button type="danger" size="large" icon="back">危险</mt-button>
+      </p>
+    </router-link>
+    <p>
+      <mt-button type="primary" size="normal">
+        <img src="../../assets/shuaxin.png" alt slot="icon" />
+        重新加载
+      </mt-button>
+    </p>
+    <p>
+      <mt-button type="primary" size="normal" icon='iconfont icon-delete' class='zdyicon'>
+        自定义图标
+      </mt-button>
+    </p>
+  </div>
 </template>
+<script>
+import '../../assets/font_rdkgeqhf1k/iconfont.css'
+export default {
+    
+}
+</script>
