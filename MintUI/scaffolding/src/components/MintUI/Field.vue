a5c1fe90ce9157bd08a07a5cb95f0dc66b31dfb3
icecream4k <969666859@qq.com>
2020-06-22 16:42:54 +0800

    6,22

--- MintUI/scaffolding/src/components/MintUI/Field.vue
+++ MintUI/scaffolding/src/components/MintUI/Field.vue
@@ -4,15 +4,42 @@
                 label='用户名' 
                 placeholder="最大长度为10" 
                 state='success' 
-                :attr="{maxlength:'10'}"></mt-field>
+                :attr="{maxlength:'10'}"
+                v-model="username"></mt-field>
+
         <mt-field type='password' 
                 label='密码' 
                 placeholder='请输入密码' 
                 state='error'
-                :attr="{maxlength:'10',autocomplete:'off'}"></mt-field>
+                :attr="{maxlength:'10',autocomplete:'off'}"
+                v-model="password"></mt-field>
+                
         <mt-field type='password' 
                 label='确认密码' 
                 placeholder='请再次输入密码' 
-                state='warning'></mt-field>
+                state='warning'
+                disableClear
+                v-model="conpassword"></mt-field>
+        <mt-button type='primary' size='large' @click="register">
+                免费注册
+        </mt-button>
     </div>
 </template>
+<script>
+export default {
+        data(){
+                return{
+                        username:'',
+                        password:'',
+                        conpassword:''
+                }
+        },
+        methods:{
+                register(){
+                        console.log('用户名>>'+this.username)
+                        console.log('密码>>'+this.password)
+                        console.log('确认密码>>'+this.conpassword)
+                }
+        }
+}
+</script>
--- vue/脚手架/xzvue/src/views/Index.vue
+++ vue/脚手架/xzvue/src/views/Index.vue
@@ -3,14 +3,18 @@
         <h1>这是首页</h1>
     </div>
 </template>
-<script>
-        // axios.defaults.baseURL='http://wrongmaker.top/';
-        // axios.get('/json.php',{params:{'appkey':'uNIKUq+qSeRcshQufXsxk5khRF4AAAAADvkmRJ1HL4sAAAAAAAAAAA==','json':'13221069695'}}).then(result=>{
-        //     console.log(result.data)
-        // })
-        // axios.post('/query',
-        //     {type:'yunda',postid:'4304926503813'}).then(result=>{
-        //         console.log(result.data);
-        //     })
+<script >
+export default {}
+import axios from '../../static/utils/axios.min.js'
 
+axios.defaults.baseURL='http://wrongmaker.top/';
+axios.get('/json.php',
+    {params:
+        {
+            'appkey':'uNIKUq+qSeRcshQufXsxk5khRF4AAAAADvkmRJ1HL4sAAAAAAAAAAA==',
+            'json':'{"phone":"13758835575"}'
+        }}).then(result=>{
+							console.log(result.data)
+                        })
+// http://wrongmaker.top/json.php?appkey=uNIKUq+qSeRcshQufXsxk5khRF4AAAAADvkmRJ1HL4sAAAAAAAAAAA==&json={"phone":"13758835575"}
 </script>
