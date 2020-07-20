f042e633feb1243dc626165c952923ae4bfdeb62
icecream4k <969666859@qq.com>
2020-07-20 23:50:23 +0800

    7.20

--- MintUI/scaffolding/src/components/MintUI/Tabcontainer.vue
+++ MintUI/scaffolding/src/components/MintUI/Tabcontainer.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <!-- 顶部导航导航开始 -->
-        <mt-navbar v-model="active">
+        <mt-navbar v-model="active" v-if='isLogin==false'>
             <mt-tab-item id="html">
                 <img src="../../../../../logo/html.png" alt slot="icon" />
             </mt-tab-item>
@@ -15,6 +15,7 @@
         <!-- 顶部导航导航结束 -->
         <!-- 中部分页栏开始 -->
         <div class="main_div">
+            
             <mt-header title="tabcontainer demo" fixed>
                 <router-link to="/" slot="left">
                     <mt-button icon="back"></mt-button>
@@ -24,7 +25,7 @@
                 </router-link>
             </mt-header>
             <div class="abcontainer_div01"></div>
-            <mt-tab-container v-model="active" swipeable>
+            <mt-tab-container v-model="active" swipeable v-if='isLogin==false'>
                 <mt-tab-container-item id="html">AAA</mt-tab-container-item>
                 <mt-tab-container-item id="css">BBB</mt-tab-container-item>
                 <mt-tab-container-item id="js">CCC</mt-tab-container-item>
@@ -34,17 +35,17 @@
         <!-- 底部选项卡 -->
         <mt-tabbar v-model="selectedTab" fixed>
             <mt-tab-item id="home">
-                <img src="../../assets/shouye.png" alt slot="icon" v-if="selectedTab == 'home'" />
-                <img src="../../assets/disabled/shouye_disabled.png" alt slot="icon" v-else />
+                <img src="../../assets/shouye.png" alt slot="icon" v-if="selectedTab == 'home'"  @click=logout() />
+                <img src="../../assets/disabled/shouye_disabled.png" alt slot="icon" v-else/>
                 首页
             </mt-tab-item>
             <mt-tab-item id="cart">
-                <img src="../../assets/gouwuche.png" alt slot="icon" v-if="selectedTab == 'cart'" />
+                <img src="../../assets/gouwuche.png" alt slot="icon" v-if="selectedTab == 'cart'" @click=login() />
                 <img src="../../assets/disabled/gouwuche_disabled.png" alt slot="icon" v-else />
                 购物车
             </mt-tab-item>
             <mt-tab-item id="me">
-                <img src="../../assets/wode.png" alt slot="icon" v-if="selectedTab == 'me'" />
+                <img src="../../assets/wode.png" alt slot="icon" v-if="selectedTab == 'me'" @click=login() />
                 <img src="../../assets/disabled/wode_disabled.png" alt slot="icon" v-else />
                 我的
             </mt-tab-item>
@@ -53,13 +54,23 @@
 </template>
 <script>
 export default {
-    data() {
+    data(){
         return {
             active: "html",
             selectedTab: "home",
             main_test: "",
-            a:''
+            a: "",
+            isLogin: false
         };
+        
+    },
+    methods: {
+        login() {
+            this.isLogin = true;
+        },
+        logout() {
+            this.isLogin = false;
+        }
     }
 };
 </script>
