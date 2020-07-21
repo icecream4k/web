c66b0c95770817279364082628c6f2d425347717
icecream4k <969666859@qq.com>
2020-07-22 00:37:46 +0800

    07-22

---
+++ git.py
@@ -0,0 +1,8 @@
+import os,datetime,time
+time_1 = str(datetime.datetime.now())
+os.system('git add *')
+time.sleep(2)
+os.system(f'git commit -m "{time_1[5:10]}"')
+time.sleep(2)
+os.system('git push')
+  
