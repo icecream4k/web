de79aac8797cf4dd52dee5943467fe289b001ffe
icecream4k <969666859@qq.com>
2020-07-29 22:59:20 +0800

    07-29

--- Html_Basic/js/myjs.js
+++ Html_Basic/js/myjs.js
@@ -1,30 +1,30 @@
-function saoma_01() {
-  let c = document.getElementById("denglu_div_01");
-  c.innerHTML = `
-	<iframe src="../html/denglu_01.html" scrolling="no" height="350px"
-	 width="355px"
-	 frameborder="0"
-	 margin-left:-5px
-	 align='center'></iframe>
-	`;
-}
-
-function saoma_02() {
-  let c = document.getElementById("denglu_div_01");
-  c.innerHTML = `
-	<iframe src="../html/denglu_02.html" scrolling="no" height="350px"
-	 width="355px"
-	 frameborder="0"
-	 margin-left:-5px
-	 align='center'></iframe>
-	`;
-}
+// function saoma_01() {
+//   let c = document.getElementById("denglu_div_01");
+//   c.innerHTML = `
+// 	<iframe src="../html/denglu_01.html" scrolling="no" height="350px"
+// 	 width="355px"
+// 	 frameborder="0"
+// 	 margin-left:-5px
+// 	 align='center'></iframe>
+// 	`;
+// }
+//
+// function saoma_02() {
+//   let c = document.getElementById("denglu_div_01");
+//   c.innerHTML = `
+// 	<iframe src="../html/denglu_02.html" scrolling="no" height="350px"
+// 	 width="355px"
+// 	 frameborder="0"
+// 	 margin-left:-5px
+// 	 align='center'></iframe>
+// 	`;
+// }
 // --------------------------
 console.log('hello world');
 // --------------------------
 var x = 1;
 var y = 1;
-console.log('line 25-27>>> ','','x',x,'y',y)
+console.log('line 25-27>>> ','x',x,'y',y)
 // --------------------------
 var eid = 3;
 var ename = 'tom';
@@ -36,7 +36,7 @@ console.log('line 29-35>>> ','eid:',eid,'ename:',ename,'sex:',sex,'birthday:',bi
 // --------------------------
 var d;
 d = 1;
-console.log(d)
+console.log('line 37-39>>>',d)
 // --------------------------
 
 
