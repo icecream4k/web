400f4be6968ef1607734161e086444461290dd2c
icecream4k <969666859@qq.com>
2020-07-31 16:41:50 +0800

    07-31

--- Html_Basic/js/myjs.js
+++ Html_Basic/js/myjs.js
@@ -20,24 +20,50 @@
 // 	`;
 // }
 // --------------------------
-console.log('hello world');
+console.log("hello world");
 // --------------------------
 var x = 1;
 var y = 1;
-console.log('line 25-27>>> ','x',x,'y',y)
+console.log("line 25-27>>> ", "x", x, "y", y);
 // --------------------------
 var eid = 3;
-var ename = 'tom';
+var ename = "tom";
 var sex = 1;
-var birthday = '1990-2-1';
+var birthday = "1990-2-1";
 var salary = 8000;
 var deptId = 20;
-console.log('line 29-35>>> ','eid:',eid,'ename:',ename,'sex:',sex,'birthday:',birthday,'salary:',salary,'deptId:',deptId)
+console.log(
+  "line 29-35>>> ",
+  "eid:",
+  eid,
+  "ename:",
+  ename,
+  "sex:",
+  sex,
+  "birthday:",
+  birthday,
+  "salary:",
+  salary,
+  "deptId:",
+  deptId
+);
 // --------------------------
 var d;
 d = 1;
-console.log('line 37-39>>>',d)
+console.log("line 37-39>>>", d);
 // --------------------------
-
-
-
+var f1 = 3.14;
+console.log("line 41-42>>>", "f1的数据类型是:", typeof f1);
+// --------------------------
+var a = "韩";
+console.log("line 44-45>>>", "韩的unicode编码是:", a.charCodeAt());
+// --------------------------
+var a = 1,
+  b = true,
+  c = "han";
+result1 = a + b + c;
+result2 = b + c + a;
+result3 = c + a + b;
+console.log('a + b + c的结果是',result1,'类型是:',typeof result1);
+console.log('b + c + a的结果是',result2,'类型是:',typeof result2);
+console.log('c + a + b的结果是',result3,'类型是:',typeof result3);
