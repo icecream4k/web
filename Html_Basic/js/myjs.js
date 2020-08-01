dd99d59282451e698a09985fa6728de59ed2568d
icecream4k <969666859@qq.com>
2020-08-01 14:31:06 +0800

    08-01

--- Html_Basic/js/myjs.js
+++ Html_Basic/js/myjs.js
@@ -24,7 +24,7 @@ console.log("hello world");
 // --------------------------
 var x = 1;
 var y = 1;
-console.log("line 25-27>>> ", "x", x, "y", y);
+console.log("x", x, "y", y);
 // --------------------------
 var eid = 3;
 var ename = "tom";
@@ -33,7 +33,6 @@ var birthday = "1990-2-1";
 var salary = 8000;
 var deptId = 20;
 console.log(
-  "line 29-35>>> ",
   "eid:",
   eid,
   "ename:",
@@ -50,13 +49,13 @@ console.log(
 // --------------------------
 var d;
 d = 1;
-console.log("line 37-39>>>", d);
+console.log(d);
 // --------------------------
 var f1 = 3.14;
-console.log("line 41-42>>>", "f1的数据类型是:", typeof f1);
+console.log("f1的数据类型是:", typeof f1);
 // --------------------------
 var a = "韩";
-console.log("line 44-45>>>", "韩的unicode编码是:", a.charCodeAt());
+console.log("韩的unicode编码是:", a.charCodeAt());
 // --------------------------
 var a = 1,
   b = true,
@@ -64,29 +63,35 @@ var a = 1,
 result1 = a + b + c;
 result2 = b + c + a;
 result3 = c + a + b;
-console.log(
-  "line 61-66>>>",
-  "a + b + c的结果是",
-  result1,
-  "类型是:",
-  typeof result1
-);
-console.log(
-  "line 61-66>>>",
-  "b + c + a的结果是",
-  result2,
-  "类型是:",
-  typeof result2
-);
-console.log(
-  "line 61-66>>>",
-  "c + a + b的结果是",
-  result3,
-  "类型是:",
-  typeof result3
-);
+console.log("a + b + c的结果是", result1, "类型是:", typeof result1);
+console.log("b + c + a的结果是", result2, "类型是:", typeof result2);
+console.log("c + a + b的结果是", result3, "类型是:", typeof result3);
 // --------------------------
-// 假设已经从数据库中获取到了商品的标题和价格，把这些数据打印成一行
+// 假设已经从数据库中获取到了商品的标题和价格,把这些数据打印成一行
 var title = "macbook pro";
 var price = 15999;
-console.log("line 72-73>>>", "商品名:", title, "价格:", price);
+console.log("商品名:", title, "价格:", price);
+// --------------------------
+// 查看一个人的工资是否在5K-7K之间
+var salary = 8000;
+console.log(salary > 5000 && salary < 7000);
+// --------------------------
+// 声明变量分别保存用户名和密码,如果用户名是root并且密码是123456,打印true,否则打印false
+var uname = "root";
+var upwd = 123456;
+console.log(uname === "root" && upwd === 123456);
+// --------------------------
+// 查看一个人是否符合免票的标准-老人或者儿童,是否满足60岁以上或者12岁以下
+var age = 31;
+console.log(age >= 60 || age <= 12);
+// --------------------------
+// 声明一个变量保存用户输入的值,如果输入的是用户名root,或者邮箱user@jd.com,或者手机号码13000000000,打印true,否则打印false
+var uname = "root";
+console.log(
+  uname === "root" || uname === "user@jd.com" || uname === "13000000000"
+);
+// --------------------------
+// 声明变量保存人的年龄，如果满18岁，打印成年人
+var age = 24;
+age > 18 && console.log('成年人');
+// --------------------------
