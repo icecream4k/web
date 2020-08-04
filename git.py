3175198de78b38922d8f52933d24d6c1713b0dae
icecream4k <969666859@qq.com>
2020-08-04 23:33:43 +0800

    08-04

--- git.py
+++ git.py
@@ -1,9 +1,9 @@
 import os,datetime,time
 def run_this(time_1):
 	os.system('git add *')
-	time.sleep(1)
+	# time.sleep(1)
 	os.system(f'git commit -m "{time_1[5:10]}"')
-	time.sleep(1)
-	os.system('git push')
+	# time.sleep(1)
+	os.system('git push -u origin master')
 time_1 = str(datetime.datetime.now())
 run_this(time_1)
