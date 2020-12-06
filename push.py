f679c464467eb81f3d76698b8cfa857ec3d93871
icecream4k <969666859@qq.com>
2020-12-07 01:12:02 +0800

    12-07

--- push.py
+++ push.py
@@ -5,9 +5,7 @@ import time
 
 def run_this(time_1):
     os.system('git add *')
-    time.sleep(1)
     os.system(f"git commit -m '{time_1[5:10]}'")
-    time.sleep(1)
     os.system('git push origin master')
 
 
