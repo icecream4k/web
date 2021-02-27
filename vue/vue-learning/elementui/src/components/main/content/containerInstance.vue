944664336dc72720799c1ec9288eafd0d9506ce8
ICEcream4K <969666859@qq.com>
2021-02-27 23:31:41 +0800

    02-27

--- vue/vue-learning/elementui/src/components/main/content/containerInstance.vue
+++ vue/vue-learning/elementui/src/components/main/content/containerInstance.vue
@@ -11,8 +11,8 @@
                             <el-menu-item index="1-2">选项2</el-menu-item>
                         </el-menu-item-group>
                         <el-menu-item-group title="分组2">
-                            <el-menu-item index="1-3">选项3</el-menu-item>
                         </el-menu-item-group>
+                            <el-menu-item index="1-3">选项3</el-menu-item>
                         <el-submenu index="1-4">
                             <template slot="title">选项4</template>
                             <el-menu-item index="1-4-1">选项4-1</el-menu-item>
