d6b6394d8530082bc7931f29afec4dadd53333f9
icecream4k <969666859@qq.com>
2021-03-29 23:37:32 +0800

    03-29

--- vue/vue-learning/test/src/components/text1.vue
+++ vue/vue-learning/test/src/components/text1.vue
@@ -1,55 +1,52 @@
 <template>
-    <div class="test1">
-        <input type="button" value="显示模态" @click="showD">
-        <div v-show="show" class="d1">
-            <div class="d2">
-                <span @click="noShowD">close</span>
-                <p>这里是模态对话框的内容,点击close取消模态</p>
-            </div>
-        </div>
+    <div class="player-container">
+        <video-player class="vjs-custom-skin" :options="playerOptions"></video-player>
     </div>
 </template>
+
 <script>
+    //引入video样式
+    import 'video.js/dist/video-js.css'
+    import 'vue-video-player/src/custom-theme.css'
+
+    //引入hls.js
+    import 'videojs-contrib-hls.js/src/videojs.hlsjs'
+
     export default {
-        name: 'test1',
         data() {
             return {
-                show: false
+                playerOptions: {
+                    playbackRates: [0.7, 1.0, 1.5, 2.0], //播放速度
+                    autoplay: true, //如果true,浏览器准备好时开始回放。
+                    controls: false, //控制条
+                    preload: 'auto', //视频预加载
+                    muted: false, //默认情况下将会消除任何音频。
+                    loop: false, //导致视频一结束就重新开始。
+                    language: 'zh-CN',
+                    aspectRatio: '16:9', // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
+                    fluid: true, // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
+                    sources: [{
+                        type: 'application/x-mpegURL',
+                        // src: 'http://60.30.52.41:80/live/zxldd.stream/playlist.m3u8',
+                        src: 'http://192.168.4.108:80/live/cameraid/1000036%240/substream/1.m3u8'
+                    }],
+                    poster: "", //你的封面地址
+                    width: document.documentElement.clientWidth,
+                    notSupportedMessage: '此视频暂无法播放，请稍后再试' //允许覆盖Video.js无法播放媒体源时显示的默认信息。
+                }
             }
         },
         methods: {
-            showD() {
-                this.show = true
-            },
-            noShowD() {
-                this.show = false
-            }
-        },
-    };
-</script>
-<style lang='less'>
-    .d1 {
-        width: 100vw;
-        height: 100vh;
-        top: 0;
-        left: 0;
-        position: absolute;
-        background-color: rgba(31, 31, 31, 0.7);
 
-        .d2 {
-            width: 400px;
-            height: 400px;
-            border-radius: 5%;
-            margin: 10% auto 0 auto;
-            background-color: whitesmoke;
-            
-            span {
-                cursor: pointer;
-                transition: all 0.3s linear;
-                &:hover {
-                    color: crimson;
-                }
-            }
+        },
+        computed: {
+
         }
+    }
+</script>
+<style lang="less">
+    .vjs-custom-skin {
+        width: 500px;
+        height: 500px;
     }
 </style>
--- vue/vue-learning/test/src/main.js
+++ vue/vue-learning/test/src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import VueVideoPlayer from 'vue-video-player'
+Vue.use(VueVideoPlayer)
 Vue.config.productionTip = false
 
 new Vue({
