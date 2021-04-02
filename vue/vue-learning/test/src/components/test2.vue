a0dd1638c843280b6caa60cf97bda13865522125
icecream4k <969666859@qq.com>
2021-04-02 15:07:01 +0800

    04-02

---
+++ vue/vue-learning/test/src/components/test2.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="test2">
+        <img src="" alt="" id="img" style="width: 400px;height: 400px;">
+    </div>
+</template>
+<script>
+    import $ from 'jquery'
+    export default {
+        name: 'test2',
+        methods: {
+            startWS() {
+                let ws = new WebSocket("ws://127.0.0.1:8124");
+                ws.onmessage = function(message) {
+                    $("#img").attr("src", message.data);
+                };
+            }
+        },
+        mounted() {
+            this.startWS()
+        },
+    };
+</script>
+<style lang='less' scoped>
+
+</style>
--- vue/vue-learning/test/src/router/index.js
+++ vue/vue-learning/test/src/router/index.js
@@ -4,31 +4,35 @@ import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-      path:'/test1',
-      name:'test',
-      component:()=>import('../components/text1.vue')
-  }
+const routes = [{
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import( /* webpackChunkName: "about" */ '../views/About.vue')
+    },
+    {
+        path: '/test1',
+        name: 'test',
+        component: () => import('../components/text1.vue')
+    },
+    {
+        path: '/test2',
+        name: 'test2',
+        component: () => import('../components/test2.vue')
+    }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
