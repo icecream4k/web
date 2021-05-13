ab79f0867796decd9cd1aabba345701a6101654f
ICEcream4K <969666859@qq.com>
2021-05-13 20:08:46 +0800

    05-13

---
+++ vue/learning/zuoye/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="app">
+        <router-view />
+
+    </div>
+</template>
+<script>
+    export default {
+        methods: {
+            data() {
+                return {
+                    isActive: false
+                }
+            },
+            change() {
+
+            }
+        },
+    }
+</script>
+<style lang="less">
+    * {
+        margin: 0;
+        padding: 0;
+    }
+
+
+
+    #app {
+        width: 100vw;
+        height: 100vh;
+    }
+</style>
